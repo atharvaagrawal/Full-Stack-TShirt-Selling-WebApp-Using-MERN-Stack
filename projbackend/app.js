c2958cc01e4d66c32c62fcbd479ad568ed7d3890
Atharva Agrawal <atharva7199990@gmail.com>
2023-01-31 19:04:08 +0530

    Added Catgory

--- projbackend/app.js
+++ projbackend/app.js
@@ -8,6 +8,8 @@ const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
 
 // DB Connection
 mongoose
@@ -28,6 +30,8 @@ app.use(cors());
 // My Routes
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
 // PORT
 const port = process.env.PORT || 8000;
